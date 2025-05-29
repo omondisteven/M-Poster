e452605b308e750c0f2a25b2c24b131a23062699
omondisteven <omondisteven@gmail.com>
2025-05-29 23:49:58 +0300

    fixing sticky header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 py-2 px-4 sm:px-6 lg:px-8 bg-white shadow-sm md:hidden">
-      <div className="max-w-7xl mx-auto flex-col justify-center flex items-center">
+    <header className="sticky top-0 z-50 bg-white shadow-sm h-16 md:hidden">
+      <div className="flex flex-col justify-center items-center h-full px-4 sm:px-6 lg:px-8">
         <h1 className="text-2xl font-display sm:text-3xl font-bold text-green-600">M-Poster</h1>
-        <h3 className="text-md font-display text-gray-800 mt-2 max-w-md">Your M-Pesa Payment Poster</h3>
+        <h3 className="text-md font-display text-gray-800 mt-1 max-w-md text-center">Your M-Pesa Payment Poster</h3>
       </div>
     </header>
   );
