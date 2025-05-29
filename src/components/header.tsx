497d2fee8c56731e81433ddb9d3d7b140ae8bd87
omondisteven <omondisteven@gmail.com>
2025-05-29 23:41:24 +0300

    fixing header padding

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 py-2 px-4 sm:px-6 lg:px-8 bg-white shadow-sm md:hidden">
-      <div className="max-w-7xl mx-auto flex-col justify-center flex items-center">
+    <header className="w-full h-16 py-2 px-4 sm:px-6 lg:px-8 bg-white shadow-sm md:hidden sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto flex-col justify-center flex items-center h-full">
         <h1 className="text-2xl font-display sm:text-3xl font-bold text-green-600">M-Poster</h1>
         <h3 className="text-md font-display text-gray-800 mt-2 max-w-md">Your M-Pesa Payment Poster</h3>
       </div>
